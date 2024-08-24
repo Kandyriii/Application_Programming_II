88693f7a025b51b97569a704ee056f79f94c9ec9
Kandyriii <pmugo2002@gmail.com>
2024-08-24 16:50:57 +0300

    my question 1

---
+++ Question1.cs
@@ -0,0 +1,14 @@
+class Calculator
+{
+static void Main(string[] args)
+{
+Console.Write("Enter the first number: ");
+double num1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the second number: ");
+double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Addition: " + (num1 + num2));
+Console.`WriteLine("Subtraction: " + (num1 num2));
+Console.WriteLine("Multiplication: " + (num1 num2));
+Console.WriteLine("Division: " + (num1 / num2));
+}
+}
